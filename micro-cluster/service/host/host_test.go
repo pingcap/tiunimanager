4c721f212b248ada432c4ca8e681f51681788a61
Jarvis Zheng <jiayang@hust.edu.cn>
2021-08-17 17:34:43 +0800

    [Host Manager] Add Unit Test Cases for host Remove

--- micro-cluster/service/host/host_test.go
+++ micro-cluster/service/host/host_test.go
@@ -213,3 +213,174 @@ func Test_ImportHostsInBatch_WithErrCode(t *testing.T) {
 		}
 	}
 }
+
+func Test_RemoveHost_Succeed(t *testing.T) {
+	initTestLog()
+	fake_str := "remove succeed"
+	fake_hostId := "xxxx-xxxx-yyyy-yyyy"
+	fakeDBClient := InitMockDBClient()
+	fakeDBClient.MockRemoveHost(func(ctx context.Context, in *db.DBRemoveHostRequest, opts ...client.CallOption) (*db.DBRemoveHostResponse, error) {
+		if in.HostId == fake_hostId {
+			rsp := new(db.DBRemoveHostResponse)
+			rsp.Rs = new(db.DBHostResponseStatus)
+			rsp.Rs.Code = int32(codes.OK)
+			rsp.Rs.Message = fake_str
+			return rsp, nil
+		} else {
+			return nil, status.Error(codes.Internal, "BAD REQUEST")
+		}
+	})
+
+	in := new(hostPb.RemoveHostRequest)
+	in.HostId = fake_hostId
+	out := new(hostPb.RemoveHostResponse)
+
+	if err := RemoveHost(context.TODO(), in, out); err != nil {
+		t.Errorf("remove host %s failed, err: %v\n", in.HostId, err)
+	}
+
+	if out.Rs.Code != int32(codes.OK) || out.Rs.Message != fake_str {
+		t.Errorf("Rsp not Expected, code: %d, msg: %s\n", out.Rs.Code, out.Rs.Message)
+	}
+}
+
+func Test_RemoveHost_WithErr(t *testing.T) {
+	initTestLog()
+	fake_hostId := "xxxx-xxxx-yyyy-yyyy"
+	fake_str := "host not exists"
+	fakeDBClient := InitMockDBClient()
+	fakeDBClient.MockRemoveHost(func(ctx context.Context, in *db.DBRemoveHostRequest, opts ...client.CallOption) (*db.DBRemoveHostResponse, error) {
+		return nil, status.Errorf(codes.NotFound, fake_str)
+	})
+
+	in := new(hostPb.RemoveHostRequest)
+	in.HostId = fake_hostId
+	out := new(hostPb.RemoveHostResponse)
+
+	if err := RemoveHost(context.TODO(), in, out); err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			if st.Code() != codes.NotFound || st.Message() != fake_str {
+				t.Errorf("Error has wrong type: code: %d, msg: %s", st.Code(), st.Message())
+			}
+		} else {
+			t.Errorf("Error not status, err: %v\n", err)
+		}
+	} else {
+		t.Errorf("Should Have a Error")
+	}
+}
+
+func Test_RemovetHost_WithErrCode(t *testing.T) {
+	initTestLog()
+	fake_hostId := "xxxx-xxxx-yyyy-yyyy"
+	fake_str := "Host Id is not Invalid"
+	fakeDBClient := InitMockDBClient()
+	fakeDBClient.MockRemoveHost(func(ctx context.Context, in *db.DBRemoveHostRequest, opts ...client.CallOption) (*db.DBRemoveHostResponse, error) {
+		rsp := new(db.DBRemoveHostResponse)
+		rsp.Rs = new(db.DBHostResponseStatus)
+		rsp.Rs.Code = int32(codes.InvalidArgument)
+		rsp.Rs.Message = fake_str
+		return rsp, nil
+	})
+
+	in := new(hostPb.RemoveHostRequest)
+	in.HostId = fake_hostId
+	out := new(hostPb.RemoveHostResponse)
+
+	if err := RemoveHost(context.TODO(), in, out); err != nil {
+		t.Errorf("Should not Have a Error")
+	} else {
+		if out.Rs.Code != int32(codes.InvalidArgument) || out.Rs.Message != fake_str {
+			t.Errorf("Error has wrong type: code: %d, msg: %s", out.Rs.Code, out.Rs.Message)
+		}
+	}
+}
+func Test_RemoveHostsInBatch_Succeed(t *testing.T) {
+	initTestLog()
+	fake_str := "remove in batch succeed"
+	fake_hostId1 := "xxxx-xxxx-yyyy-yyyy"
+	fake_hostId2 := "yyyy-yyyy-xxxx-xxxx"
+	fakeDBClient := InitMockDBClient()
+	fakeDBClient.MockRemoveHostsInBatch(func(ctx context.Context, in *db.DBRemoveHostsInBatchRequest, opts ...client.CallOption) (*db.DBRemoveHostsInBatchResponse, error) {
+		if in.HostIds[0] == fake_hostId1 && in.HostIds[1] == fake_hostId2 {
+			rsp := new(db.DBRemoveHostsInBatchResponse)
+			rsp.Rs = new(db.DBHostResponseStatus)
+			rsp.Rs.Code = int32(codes.OK)
+			rsp.Rs.Message = fake_str
+			return rsp, nil
+		} else {
+			return nil, status.Error(codes.Internal, "BAD REQUEST")
+		}
+	})
+
+	in := new(hostPb.RemoveHostsInBatchRequest)
+	in.HostIds = append(in.HostIds, fake_hostId1)
+	in.HostIds = append(in.HostIds, fake_hostId2)
+	out := new(hostPb.RemoveHostsInBatchResponse)
+
+	if err := RemoveHostsInBatch(context.TODO(), in, out); err != nil {
+		t.Errorf("remove host in batch failed, err: %v\n", err)
+	}
+
+	if out.Rs.Code != int32(codes.OK) || out.Rs.Message != fake_str {
+		t.Errorf("Rsp not Expected, code: %d, msg: %s\n", out.Rs.Code, out.Rs.Message)
+	}
+}
+
+func Test_RemoveHostsInBatch_WithErr(t *testing.T) {
+	initTestLog()
+	fake_str := "host already exists"
+	fake_hostId1 := "xxxx-xxxx-yyyy-yyyy"
+	fake_hostId2 := "yyyy-yyyy-xxxx-xxxx"
+	fakeDBClient := InitMockDBClient()
+	fakeDBClient.MockRemoveHostsInBatch(func(ctx context.Context, in *db.DBRemoveHostsInBatchRequest, opts ...client.CallOption) (*db.DBRemoveHostsInBatchResponse, error) {
+		return nil, status.Errorf(codes.NotFound, fake_str)
+	})
+
+	in := new(hostPb.RemoveHostsInBatchRequest)
+	in.HostIds = append(in.HostIds, fake_hostId1)
+	in.HostIds = append(in.HostIds, fake_hostId2)
+	out := new(hostPb.RemoveHostsInBatchResponse)
+
+	if err := RemoveHostsInBatch(context.TODO(), in, out); err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			if st.Code() != codes.NotFound || st.Message() != fake_str {
+				t.Errorf("Error has wrong type: code: %d, msg: %s", st.Code(), st.Message())
+			}
+		} else {
+			t.Errorf("Error not status, err: %v\n", err)
+		}
+	} else {
+		t.Errorf("Should Have a Error")
+	}
+}
+
+func Test_RemoveHostsInBatch_WithErrCode(t *testing.T) {
+	initTestLog()
+	fake_str := "Host Ip is not Invalid"
+	fake_hostId1 := "xxxx-xxxx-yyyy-yyyy"
+	fake_hostId2 := "yyyy-yyyy-xxxx-xxxx"
+	fakeDBClient := InitMockDBClient()
+	fakeDBClient.MockRemoveHostsInBatch(func(ctx context.Context, in *db.DBRemoveHostsInBatchRequest, opts ...client.CallOption) (*db.DBRemoveHostsInBatchResponse, error) {
+		rsp := new(db.DBRemoveHostsInBatchResponse)
+		rsp.Rs = new(db.DBHostResponseStatus)
+		rsp.Rs.Code = int32(codes.InvalidArgument)
+		rsp.Rs.Message = fake_str
+		return rsp, nil
+	})
+
+	in := new(hostPb.RemoveHostsInBatchRequest)
+	in.HostIds = append(in.HostIds, fake_hostId1)
+	in.HostIds = append(in.HostIds, fake_hostId2)
+	out := new(hostPb.RemoveHostsInBatchResponse)
+
+	if err := RemoveHostsInBatch(context.TODO(), in, out); err != nil {
+		t.Errorf("Should not Have a Error")
+	} else {
+		if out.Rs.Code != int32(codes.InvalidArgument) || out.Rs.Message != fake_str {
+			t.Errorf("Error has wrong type: code: %d, msg: %s", out.Rs.Code, out.Rs.Message)
+		}
+	}
+}

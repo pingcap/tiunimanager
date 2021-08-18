ecae40c74060d12bd77627b99fd7e52574927201
zhangpeijin-milo <zhangpeijin@pingcap.com>
2021-08-18 12:11:46 +0800

    [metadb entity]replace '/' and '+' in id

--- micro-metadb/models/idgenerator_test.go
+++ micro-metadb/models/idgenerator_test.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -13,4 +14,30 @@ func TestGenerateID(t *testing.T) {
 	if len(got) != ID_LENGTH {
 		t.Errorf("GenerateID() want len = %v, got = %v", ID_LENGTH, len(got))
 	}
+
+}
+
+func TestGenerateIDReplace(t *testing.T) {
+	time := 0
+	for time < 100 {
+		got := GenerateID()
+		if strings.Contains(got, "/") {
+			t.Errorf("GenerateID() got /")
+		}
+		if strings.Contains(got, "-") {
+			break
+		}
+		time ++
+	}
+	for time < 200 {
+		got := GenerateID()
+		if strings.Contains(got, "/") {
+			t.Errorf("GenerateID() got /")
+		}
+		if strings.Contains(got, "-") {
+			break
+		}
+		time ++
+	}
+
 }

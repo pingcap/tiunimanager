a820beeb141e95badc68abb0f7fed2f24862c983
Haibo Shen <haiboshen@Haibos-MacBook-Pro.local>
2021-08-24 10:43:11 +0800

    [secondparty][br] return error info for sync method

--- library/secondparty/libbr/libbr_test.go
+++ library/secondparty/libbr/libbr_test.go
@@ -1,6 +1,19 @@
 package libbr
 
-import "testing"
+import (
+	"fmt"
+	"github.com/pingcap/tiem/library/thirdparty/logger"
+	"testing"
+)
+
+func init() {
+	fmt.Println("init before test")
+	log = logger.GetLogger("config_key_test_log")
+	//BrMgrInit()
+	// todo: all binaries has been put under bin folder of project root path in another branch, need to change later
+	MicroInit("../../../bin/micro-cluster/brmgr/brmgr", "/tmp/log/br/")
+
+}
 
 func TestBrMgrInit(t *testing.T) {
 	BrMgrInit()
@@ -11,5 +24,4 @@ func TestBrMgrInit(t *testing.T) {
 }
 
 func TestBackUpAndInfo(t *testing.T) {
-
 }

1d4470b9e2e738d607bd6324ce4b90ce7a167c8f
Haibo Shen <haibo@umich.edu>
2021-09-03 22:46:58 +0800

    [ut] fix adding test file

--- build_helper/add_test_file.sh
+++ build_helper/add_test_file.sh
@@ -1,8 +1,8 @@
 find . -name "*.go" | grep -vE "proto|docs|library/util" | while read fname; do
     fpath=`dirname $fname`
-    firstline=$(head -n 1 $fname)
+    firstline=$(sed -n -e '/^package/p' $fname)
     if [ ! -f "$fpath/main_test.go" ]; then
       echo $firstline > $fpath/main_test.go
-      echo -e "\nimport \"testing\"\n\nfunc TestMain(m *testing.M) {}" >> $fpath/main_test.go
+      echo "\nimport \"testing\"\n\nfunc TestMain(m *testing.M) {}" >> $fpath/main_test.go
     fi
 done

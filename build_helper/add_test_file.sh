f5e6f13ceacdcd1d66357c8294085376bb33c7d1
Haibo Shen <haibo@umich.edu>
2021-09-03 23:16:25 +0800

    [ut] fix adding test file by only adding file while no test file exists

--- build_helper/add_test_file.sh
+++ build_helper/add_test_file.sh
@@ -1,8 +1,11 @@
 find . -name "*.go" | grep -vE "proto|docs|library/util" | while read fname; do
     fpath=`dirname $fname`
     firstline=$(sed -n -e '/^package/p' $fname)
-    if [ ! -f "$fpath/main_test.go" ]; then
+#    if [ ! -f "$fpath/main_test.go" ]; then
+    if ls $fpath/*_test.go 1> /dev/null 2>&1; then
+      echo "no need to add main_test.go file in $fpath"
+    else
       echo $firstline > $fpath/main_test.go
-      echo "\nimport \"testing\"\n\nfunc TestMain(m *testing.M) {}" >> $fpath/main_test.go
+      echo "import \"testing\"\nfunc TestMain(m *testing.M) {}" >> $fpath/main_test.go
     fi
 done

3587b5c5e1d46a08ff451a5f5e83c84b196cb9ff
Sen Han <00hnes@gmail.com>
2021-09-03 18:54:43 +0800

    fix opentracing

--- library/framework/tracer.go
+++ library/framework/tracer.go
@@ -8,7 +8,6 @@ import (
 	"github.com/asim/go-micro/v3/metadata"
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
@@ -61,31 +60,6 @@ func NewJaegerTracer(serviceName string, addr string) (opentracing.Tracer, io.Cl
 	)
 
 	return tracer, closer, err
-}
-
-func GinOpenTracing() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var parentSpan opentracing.Span
-
-		tracer := opentracing.GlobalTracer()
-
-		spCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
-		if err != nil {
-			parentSpan = tracer.StartSpan(c.Request.URL.Path)
-			defer parentSpan.Finish()
-		} else {
-			parentSpan = opentracing.StartSpan(
-				c.Request.URL.Path,
-				opentracing.ChildOf(spCtx),
-				opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
-				ext.SpanKindRPCServer,
-			)
-			defer parentSpan.Finish()
-		}
-		c.Set("Tracer", tracer)
-		c.Set("ParentSpan", parentSpan)
-		c.Next()
-	}
 }
 
 // make a new micro ctx base on the gin ctx
---
+++ micro-api/interceptor/opentracing.go
@@ -0,0 +1,32 @@
+package interceptor
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+func GinOpenTracing() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var parentSpan opentracing.Span
+
+		tracer := opentracing.GlobalTracer()
+
+		spCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
+		if err != nil {
+			parentSpan = tracer.StartSpan(c.Request.URL.Path)
+			defer parentSpan.Finish()
+		} else {
+			parentSpan = opentracing.StartSpan(
+				c.Request.URL.Path,
+				opentracing.ChildOf(spCtx),
+				opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
+				ext.SpanKindRPCServer,
+			)
+			defer parentSpan.Finish()
+		}
+		c.Set("Tracer", tracer)
+		c.Set("ParentSpan", parentSpan)
+		c.Next()
+	}
+}

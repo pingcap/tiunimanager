9f22b0b75ed88c3a8f15fdf152dc372d593bfdd4
Haibo Shen <haibo@umich.edu>
2021-09-06 17:21:04 +0800

    [ut] try fix cross platform for mock test

--- build_helper/add_test_file.sh
+++ build_helper/add_test_file.sh
@@ -6,6 +6,7 @@ find . -name "*.go" | grep -vE "proto|docs|library/util" | while read fname; do
       echo "no need to add main_test.go file in $fpath"
     else
       echo $firstline > $fpath/main_test.go
-      echo "import \"testing\"\nfunc Test_Main(t *testing.T) {}" >> $fpath/main_test.go
+      echo "import \"testing\"" >> $fpath/main_test.go
+      echo "func Test_Main(t *testing.T) {}" >> $fpath/main_test.go
     fi
 done

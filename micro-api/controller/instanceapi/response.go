b0115e4e7cc238d614da7c8b8c5b6d7b3054d203
chencheng <chencheng@pingcap.com>
2021-09-17 15:07:40 +0800

    gofmt

--- micro-api/controller/instanceapi/response.go
+++ micro-api/controller/instanceapi/response.go
@@ -1,7 +1,7 @@
 package instanceapi
 
 type ParamUpdateRsp struct {
-	Status 			string	`json:"status"`
-	ClusterId 		string	`json:"clusterId"`
-	TaskId			uint	`json:"taskId"`
+	Status    string `json:"status"`
+	ClusterId string `json:"clusterId"`
+	TaskId    uint   `json:"taskId"`
 }

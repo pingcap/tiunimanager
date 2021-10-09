04ff1cc7a4c5441a8c740ff833c6e15c6c137543
istudies <jiangxunyu2017@163.com>
2021-10-09 12:17:46 +0800

    edit destroy short description

--- tiup/cmd/destroy.go
+++ tiup/cmd/destroy.go
@@ -22,7 +22,7 @@ func newDestroyCmd() *cobra.Command {
 	destroyOpt := operator.Options{}
 	cmd := &cobra.Command{
 		Use:   "destroy <cluster-name>",
-		Short: "Destroy a specified DM cluster",
+		Short: "Destroy a specified TiEM cluster",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) != 1 {
 				return cmd.Help()

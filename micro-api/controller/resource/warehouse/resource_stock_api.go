972ac851d756365384f004111c29729bd1bd7c08
Jarvis Zheng <jiayang@hust.edu.cn>
2021-10-28 09:34:25 +0800

    [Resource manager] Fix gosec

--- micro-api/controller/resource/warehouse/resource_stock_api.go
+++ micro-api/controller/resource/warehouse/resource_stock_api.go
@@ -50,7 +50,7 @@ func GetFailureDomain(c *gin.Context) {
 	if domainStr == "" {
 		domain = int(resource.ZONE)
 	}
-	domain, err := strconv.Atoi(domainStr)
+	domain, err := strconv.Atoi(domainStr) // #nosec G109
 	if err != nil || domain > int(resource.RACK) || domain < int(resource.REGION) {
 		errmsg := fmt.Sprintf("Input domainType [%s] Invalid: %v", c.Query("failureDomainType"), err)
 		c.JSON(http.StatusBadRequest, controller.Fail(int(codes.InvalidArgument), errmsg))

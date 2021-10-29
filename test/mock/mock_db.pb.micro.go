0b31492541d1478f0a8b0aac7b8676f7e1b23abe
chencheng <chencheng@pingcap.com>
2021-10-29 19:13:21 +0800

    fix mock

--- test/mock/mock_db.pb.micro.go
+++ test/mock/mock_db.pb.micro.go
@@ -19,10 +19,6 @@ type MockTiEMDBService struct {
 	recorder *MockTiEMDBServiceMockRecorder
 }
 
-func (m *MockTiEMDBService) UpdateClusterInfo(ctx context.Context, in *dbpb.DBUpdateClusterInfoRequest, opts ...client.CallOption) (*dbpb.DBUpdateClusterInfoResponse, error) {
-	panic("implement me")
-}
-
 // MockTiEMDBServiceMockRecorder is the mock recorder for MockTiEMDBService.
 type MockTiEMDBServiceMockRecorder struct {
 	mock *MockTiEMDBService

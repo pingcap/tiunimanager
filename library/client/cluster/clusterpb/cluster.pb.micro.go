035eb9312384c42a3448200d1d618d9ad5b97fb6
zhangpeijin-milo <zhangpeijin@pingcap.com>
2021-11-01 18:06:26 +0800

    [pb]

--- library/client/cluster/clusterpb/cluster.pb.micro.go
+++ library/client/cluster/clusterpb/cluster.pb.micro.go
@@ -5,16 +5,14 @@ package clusterpb
 
 import (
 	fmt "fmt"
-	math "math"
-
 	proto "github.com/golang/protobuf/proto"
+	math "math"
+)
 
+import (
 	context "context"
-
 	api "github.com/asim/go-micro/v3/api"
-
 	client "github.com/asim/go-micro/v3/client"
-
 	server "github.com/asim/go-micro/v3/server"
 )
 
@@ -22,6 +20,12 @@ import (
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ api.Endpoint

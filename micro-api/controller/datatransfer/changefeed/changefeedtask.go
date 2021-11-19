b71a3eda7c5442b635708993316f12cbcaa4a625
zhangpeijin-milo <zhangpeijin@pingcap.com>
2021-11-19 17:04:16 +0800

    [changefeed]struct

---
+++ micro-api/controller/datatransfer/changefeed/changefeedtask.go
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (c)  2021 PingCAP, Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+package changefeed
+
+type ChangeFeedTask struct {
+	Id string
+	StartTSO uint
+	Rules []string
+	Status int
+	DownstreamType int
+	DownstreamConfig interface{}
+}
+
+type ChangeFeedTaskCreateReq struct {
+	ChangeFeedTask
+	Id string
+	Url string
+	Port string
+}

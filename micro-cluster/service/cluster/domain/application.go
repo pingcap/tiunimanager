487057bd38d42b7c0e48cd30bf26ef7c9320f5de
zhangpeijin-milo <zhangpeijin@pingcap.com>
2021-11-19 20:11:30 +0800

    [cluster]taskinfo

--- micro-cluster/service/cluster/domain/application.go
+++ micro-cluster/service/cluster/domain/application.go
@@ -504,9 +504,9 @@ func buildConfig(task *TaskEntity, context *FlowContext) bool {
 	clusterAggregation.AlteredTopology = configModel
 	bytes, err := yaml.Marshal(configModel)
 	if err != nil {
-		task.Success(string(bytes))
-	} else {
 		task.Success(nil)
+	} else {
+		task.Success(string(bytes))
 	}
 	return true
 }

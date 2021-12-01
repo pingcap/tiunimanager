529f8fb28a72df32c12afe1b5fd000f0d7474142
zhangpeijin-milo <zhangpeijin@pingcap.com>
2021-12-01 14:45:14 +0800

    [package]

--- apimodels/cluster/performance/models.go
+++ apimodels/cluster/performance/models.go
@@ -13,4 +13,4 @@
  * limitations under the License.                                             *
  ******************************************************************************/
 
-package alert
+package performance

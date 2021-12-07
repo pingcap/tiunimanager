8f6726a6a598fe37bbab48f8809d8487ab4f1563
chencheng <chencheng@pingcap.com>
2021-12-07 09:57:15 +0800

    modify api interface

---
+++ micro-cluster/cluster/backuprestore/service.go
@@ -0,0 +1,95 @@
+/******************************************************************************
+ * Copyright (c)  2021 PingCAP, Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package backuprestore
+
+import (
+	"context"
+	"github.com/pingcap-inc/tiem/message/cluster"
+)
+
+type BRApi interface {
+	// BackupCluster
+	// @Description: backup cluster
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.BackupClusterDataResp
+	// @Return error
+	BackupCluster(ctx context.Context, request *cluster.BackupClusterDataReq) (*cluster.BackupClusterDataResp, error)
+
+	// RestoreNewCluster
+	// @Description: restore a new cluster by backup record
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.RestoreNewClusterResp
+	// @Return error
+	//RestoreNewCluster(ctx context.Context, request *cluster.RestoreNewClusterReq) (*cluster.RestoreNewClusterResp, error) // todo: move to cluster manager to reuse create workflow node
+
+	// RestoreExistCluster
+	// @Description: restore exist cluster by backup record
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.RestoreExistClusterResp
+	// @Return error
+	RestoreExistCluster(ctx context.Context, request *cluster.RestoreExistClusterReq) (*cluster.RestoreExistClusterResp, error)
+
+	// QueryClusterBackupRecords
+	// @Description: query backup records of cluster
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.QueryBackupRecordsResp
+	// @Return error
+	QueryClusterBackupRecords(ctx context.Context, request *cluster.QueryBackupRecordsReq) (*cluster.QueryBackupRecordsResp, error)
+
+	// DeleteBackupRecords
+	// @Description: delete backup records by condition
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.QueryBackupRecordsResp
+	// @Return error
+	DeleteBackupRecords(ctx context.Context, request *cluster.DeleteBackupDataReq) (*cluster.DeleteBackupDataResp, error)
+
+	// QueryBackupStrategy
+	// @Description: query backup strategy of cluster
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.QueryBackupStrategyResp
+	// @Return error
+	QueryBackupStrategy(ctx context.Context, request *cluster.QueryBackupStrategyReq) (*cluster.QueryBackupStrategyResp, error)
+
+	// SaveBackupStrategy
+	// @Description: save backup strategy of cluster
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.UpdateBackupStrategyResp
+	// @Return error
+	SaveBackupStrategy(ctx context.Context, request *cluster.UpdateBackupStrategyReq) (*cluster.UpdateBackupStrategyResp, error)
+
+	// DeleteBackupStrategy
+	// @Description: save backup strategy of cluster
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *cluster.UpdateBackupStrategyResp
+	// @Return error
+	DeleteBackupStrategy(ctx context.Context, request *cluster.DeleteBackupStrategyReq) (*cluster.DeleteBackupStrategyResp, error)
+}
---
+++ micro-cluster/datatransfer/importexport/service.go
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * Copyright (c)  2021 PingCAP, Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package importexport
+
+import (
+	"context"
+	"github.com/pingcap-inc/tiem/message"
+)
+
+type ImportExportApi interface {
+	// ExportData
+	// @Description: export data
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *message.DataExportResp
+	// @Return error
+	ExportData(ctx context.Context, request *message.DataExportReq) (*message.DataExportResp, error)
+
+	// ImportData
+	// @Description: import data
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *message.DataImportResp
+	// @Return error
+	ImportData(ctx context.Context, request *message.DataImportReq) (*message.DataImportResp, error)
+
+	// QueryDataTransportRecords
+	// @Description: query data import & export records by condition
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *message.QueryDataImportExportRecordsResp
+	// @Return error
+	QueryDataTransportRecords(ctx context.Context, request *message.QueryDataImportExportRecordsReq) (*message.QueryDataImportExportRecordsResp, error)
+
+	// DeleteDataTransportRecord
+	// @Description: delete data import & export records by condition
+	// @Receiver m
+	// @Parameter ctx
+	// @Parameter request
+	// @Return *message.DeleteImportExportRecordResp
+	// @Return error
+	DeleteDataTransportRecord(ctx context.Context, request *message.DeleteImportExportRecordReq) (*message.DeleteImportExportRecordResp, error)
+}

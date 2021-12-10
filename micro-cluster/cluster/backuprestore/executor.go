af858b237c1a33c9b9ef347854c50d52c921f54f
chencheng <chencheng@pingcap.com>
2021-12-10 17:30:54 +0800

    move task node to executor

---
+++ micro-cluster/cluster/backuprestore/executor.go
@@ -0,0 +1,192 @@
+/******************************************************************************
+ * Copyright (c)  2021 PingCAP, Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package backuprestore
+
+import (
+	"fmt"
+	"github.com/pingcap-inc/tiem/common/constants"
+	"github.com/pingcap-inc/tiem/library/framework"
+	"github.com/pingcap-inc/tiem/library/secondparty"
+	"github.com/pingcap-inc/tiem/micro-cluster/cluster/management/handler"
+	"github.com/pingcap-inc/tiem/models"
+	"github.com/pingcap-inc/tiem/models/cluster/backuprestore"
+	wfModel "github.com/pingcap-inc/tiem/models/workflow"
+	"github.com/pingcap-inc/tiem/workflow"
+	"strconv"
+	"time"
+)
+
+func backupCluster(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin backupCluster")
+	defer framework.LogWithContext(ctx).Info("end backupCluster")
+
+	record := ctx.GetData(contextBackupRecordKey).(*backuprestore.BackupRecord)
+	meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+
+	//todo: get from meta
+	tidbServerHost := ""
+	tidbServerPort := 0
+	if tidbServerPort == 0 {
+		tidbServerPort = constants.DefaultTiDBPort
+	}
+
+	storageType, err := convertBrStorageType(record.StorageType)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("convert storage type failed, %s", err.Error())
+		node.Fail(err)
+		return false
+	}
+
+	clusterFacade := secondparty.ClusterFacade{
+		DbConnParameter: secondparty.DbConnParam{
+			Username: "root", //todo: replace admin account
+			Password: "",
+			IP:       tidbServerHost,
+			Port:     strconv.Itoa(tidbServerPort),
+		},
+		DbName:      "", //todo: support db table backup
+		TableName:   "",
+		ClusterId:   meta.GetCluster().ID,
+		ClusterName: meta.GetCluster().Name,
+	}
+	storage := secondparty.BrStorage{
+		StorageType: storageType,
+		Root:        fmt.Sprintf("%s/%s", record.FilePath, "?access-key=minioadmin\\&secret-access-key=minioadmin\\&endpoint=http://minio.pingcap.net:9000\\&force-path-style=true"), //todo: test env s3 ak sk, get from config
+	}
+
+	framework.LogWithContext(ctx).Infof("begin call brmgr backup api, clusterFacade[%v], storage[%v]", clusterFacade, storage)
+
+	backupTaskId, err := secondparty.Manager.BackUp(ctx, clusterFacade, storage, node.ID)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("call backup api failed, %s", err.Error())
+		node.Fail(err)
+		return false
+	}
+	ctx.SetData("backupTaskId", backupTaskId)
+	node.Success()
+	return true
+}
+
+func updateBackupRecord(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin updateBackupRecord")
+	defer framework.LogWithContext(ctx).Info("end updateBackupRecord")
+
+	meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+	record := ctx.GetData(contextBackupRecordKey).(*backuprestore.BackupRecord)
+
+	//todo: show backup info and update backupTSO,size,endTime
+	size := uint64(0)
+	backupTSO := uint64(0)
+	brRW := models.GetBRReaderWriter()
+	err := brRW.UpdateBackupRecord(ctx, record.ID, string(constants.ClusterBackupFinished), size, backupTSO, time.Now())
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("update backup reocrd %s of cluster %s failed", record.ID, meta.GetCluster().ID)
+		node.Fail(err)
+		return false
+	}
+
+	node.Success()
+	return true
+}
+
+func restoreFromSrcCluster(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin recoverFromSrcCluster")
+	defer framework.LogWithContext(ctx).Info("end recoverFromSrcCluster")
+
+	meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+	record := ctx.GetData(contextBackupRecordKey).(*backuprestore.BackupRecord)
+
+	//todo: get from meta
+	tidbServerHost := ""
+	tidbServerPort := 0
+	storageType, err := convertBrStorageType(record.StorageType)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("convert br storage type failed, %s", err.Error())
+		node.Fail(fmt.Errorf("convert br storage type failed, %s", err.Error()))
+		return false
+	}
+
+	clusterFacade := secondparty.ClusterFacade{
+		DbConnParameter: secondparty.DbConnParam{
+			Username: "root", //todo: replace admin account
+			Password: "",
+			IP:       tidbServerHost,
+			Port:     strconv.Itoa(tidbServerPort),
+		},
+		DbName:      "", //todo: support db table restore
+		TableName:   "",
+		ClusterId:   meta.GetCluster().ID,
+		ClusterName: meta.GetCluster().Name,
+	}
+	storage := secondparty.BrStorage{
+		StorageType: storageType,
+		Root:        fmt.Sprintf("%s/%s", record.FilePath, "?access-key=minioadmin\\&secret-access-key=minioadmin\\&endpoint=http://minio.pingcap.net:9000\\&force-path-style=true"), //todo: test env s3 ak sk
+	}
+	framework.LogWithContext(ctx).Infof("begin call brmgr restore api, clusterFacade %v, storage %v", clusterFacade, storage)
+	_, err = secondparty.Manager.Restore(ctx, clusterFacade, storage, node.ID)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("call restore api failed, %s", err.Error())
+		node.Fail(err)
+		return false
+	}
+	node.Success()
+	return true
+}
+
+func defaultEnd(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin defaultEnd")
+	defer framework.LogWithContext(ctx).Info("end defaultEnd")
+
+	node.Success()
+	return true
+}
+
+func backupFail(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin backupFail")
+	defer framework.LogWithContext(ctx).Info("end backupFail")
+
+	meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+	record := ctx.GetData(contextBackupRecordKey).(*backuprestore.BackupRecord)
+
+	brRW := models.GetBRReaderWriter()
+	err := brRW.UpdateBackupRecord(ctx, record.ID, string(constants.ClusterBackupFailed), 0, 0, time.Now())
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("update backup reocrd %s of cluster %s failed", record.ID, meta.GetCluster().ID)
+		node.Fail(err)
+		return false
+	}
+
+	node.Success()
+	return true
+}
+
+func restoreFail(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin restoreFail")
+	defer framework.LogWithContext(ctx).Info("end restoreFail")
+
+	node.Success()
+	return true
+}
+
+func convertBrStorageType(storageType string) (secondparty.StorageType, error) {
+	if string(constants.StorageTypeS3) == storageType {
+		return secondparty.StorageTypeS3, nil
+	} else if string(constants.StorageTypeLocal) == storageType {
+		return secondparty.StorageTypeLocal, nil
+	} else {
+		return "", fmt.Errorf("invalid storage type, %s", storageType)
+	}
+}
---
+++ micro-cluster/datatransfer/importexport/executor.go
@@ -0,0 +1,229 @@
+/******************************************************************************
+ * Copyright (c)  2021 PingCAP, Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ ******************************************************************************/
+
+package importexport
+
+import (
+	"context"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/pingcap-inc/tiem/common/constants"
+	"github.com/pingcap-inc/tiem/library/common"
+	"github.com/pingcap-inc/tiem/library/framework"
+	"github.com/pingcap-inc/tiem/library/secondparty"
+	"github.com/pingcap-inc/tiem/micro-cluster/cluster/management/handler"
+	"github.com/pingcap-inc/tiem/models"
+	wfModel "github.com/pingcap-inc/tiem/models/workflow"
+	"github.com/pingcap-inc/tiem/workflow"
+	"os"
+	"strconv"
+	"time"
+)
+
+func buildDataImportConfig(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin buildDataImportConfig")
+	defer framework.LogWithContext(ctx).Info("end buildDataImportConfig")
+
+	meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+	info := ctx.GetData(contextDataTransportRecordKey).(*ImportInfo)
+
+	config := NewDataImportConfig(meta, info)
+	if config == nil {
+		framework.LogWithContext(ctx).Errorf("convert toml config failed, cluster: %s", meta.GetCluster().ID)
+		node.Fail(fmt.Errorf("convert toml config failed, cluster: %s", meta.GetCluster().ID))
+		return false
+	}
+
+	filePath := fmt.Sprintf("%s/tidb-lightning.toml", info.ConfigPath)
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0600)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("create import toml config failed, %s", err.Error())
+		node.Fail(fmt.Errorf("create import toml config failed, %s", err.Error()))
+		return false
+	}
+
+	if err = toml.NewEncoder(file).Encode(config); err != nil {
+		framework.LogWithContext(ctx).Errorf("encode data import toml config failed, %s", err.Error())
+		node.Fail(fmt.Errorf("encode data import toml config failed, %s", err.Error()))
+		return false
+	}
+	framework.LogWithContext(ctx).Infof("build lightning toml config sucess, %v", config)
+	node.Success()
+	return true
+}
+
+func importDataToCluster(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin importDataToCluster")
+	defer framework.LogWithContext(ctx).Info("end importDataToCluster")
+
+	info := ctx.GetData(contextDataTransportRecordKey).(*ImportInfo)
+
+	//tiup tidb-lightning -config tidb-lightning.toml
+	importTaskId, err := secondparty.Manager.Lightning(ctx, 0,
+		[]string{"-config", fmt.Sprintf("%s/tidb-lightning.toml", info.ConfigPath)},
+		node.ID)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("call tiup lightning api failed, %s", err.Error())
+		node.Fail(fmt.Errorf("call tiup lightning api failed, %s", err.Error()))
+		return false
+	}
+	framework.LogWithContext(ctx).Infof("call tiupmgr tidb-lightning api success, importTaskId %d", importTaskId)
+	node.Success()
+	return true
+}
+
+func updateDataImportRecord(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin updateDataImportRecord")
+	defer framework.LogWithContext(ctx).Info("end updateDataImportRecord")
+
+	info := ctx.GetData(contextDataTransportRecordKey).(*ImportInfo)
+
+	rw := models.GetImportExportReaderWriter()
+	err := rw.UpdateDataTransportRecord(ctx, info.RecordId, string(constants.DataImportExportFinished), time.Now())
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("update data transport record failed, %s", err.Error())
+		node.Fail(fmt.Errorf("update data transport record failed, %s", err.Error()))
+		return false
+	}
+	framework.LogWithContext(ctx).Info("update data transport record success")
+	node.Success()
+	return true
+}
+
+func exportDataFromCluster(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin exportDataFromCluster")
+	defer framework.LogWithContext(ctx).Info("end exportDataFromCluster")
+
+	//meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+	info := ctx.GetData(contextDataTransportRecordKey).(*ExportInfo)
+
+	//todo: get from meta
+	tidbHost := ""
+	tidbPort := 4000
+	if tidbPort == 0 {
+		tidbPort = constants.DefaultTiDBPort
+	}
+
+	if common.NfsStorageType == info.StorageType {
+		if err := cleanDataTransportDir(ctx, info.FilePath); err != nil {
+			framework.LogWithContext(ctx).Errorf("clean export directory failed, %s", err.Error())
+			node.Fail(fmt.Errorf("clean export directory failed, %s", err.Error()))
+			return false
+		}
+	}
+
+	//tiup dumpling -u root -P 4000 --host 127.0.0.1 --filetype sql -t 8 -o /tmp/test -r 200000 -F 256MiB --filter "user*"
+	//todo: replace root password
+	cmd := []string{"-u", info.UserName,
+		"-p", info.Password,
+		"-P", strconv.Itoa(tidbPort),
+		"--host", tidbHost,
+		"--filetype", info.FileType,
+		"-t", "8",
+		"-o", info.FilePath,
+		"-r", "200000",
+		"-F", "256MiB"}
+	if info.Filter != "" {
+		cmd = append(cmd, "--filter", info.Filter)
+	}
+	if FileTypeCSV == info.FileType && info.Filter == "" && info.Sql != "" {
+		cmd = append(cmd, "--sql", info.Sql)
+	}
+	if common.S3StorageType == info.StorageType && info.BucketRegion != "" {
+		cmd = append(cmd, "--s3.region", fmt.Sprintf("\"%s\"", info.BucketRegion))
+	}
+	framework.LogWithContext(ctx).Infof("call tiupmgr dumpling api, cmd: %v", cmd)
+	exportTaskId, err := secondparty.Manager.Dumpling(ctx, 0, cmd, node.ID)
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("call tiup dumpling api failed, %s", err.Error())
+		node.Fail(fmt.Errorf("call tiup dumpling api failed, %s", err.Error()))
+		return false
+	}
+
+	framework.LogWithContext(ctx).Infof("call tiupmgr succee, exportTaskId: %d", exportTaskId)
+	node.Success()
+	return true
+}
+
+func updateDataExportRecord(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin updateDataExportRecord")
+	defer framework.LogWithContext(ctx).Info("end updateDataExportRecord")
+
+	info := ctx.GetData(contextDataTransportRecordKey).(*ExportInfo)
+
+	rw := models.GetImportExportReaderWriter()
+	err := rw.UpdateDataTransportRecord(ctx, info.RecordId, string(constants.DataImportExportFinished), time.Now())
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("update data transport record failed, %s", err.Error())
+		node.Fail(fmt.Errorf("update data transport record failed, %s", err.Error()))
+		return false
+	}
+
+	framework.LogWithContext(ctx).Info("update data transport record success")
+	node.Success()
+	return true
+}
+
+func importDataFailed(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	framework.LogWithContext(ctx).Info("begin importDataFailed")
+	defer framework.LogWithContext(ctx).Info("end importDataFailed")
+
+	meta := ctx.GetData(contextClusterMetaKey).(*handler.ClusterMeta)
+	info := ctx.GetData(contextDataTransportRecordKey).(*ExportInfo)
+	if err := updateTransportRecordFailed(ctx, info.RecordId, meta.GetCluster().ID); err != nil {
+		node.Fail(err)
+		return false
+	}
+
+	return clusterFail(node, ctx)
+}
+
+func exportDataFailed(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	return clusterFail(node, ctx)
+}
+
+func clusterEnd(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	node.Success()
+	return true
+}
+
+func clusterFail(node *wfModel.WorkFlowNode, ctx *workflow.FlowContext) bool {
+	node.Success()
+	return true
+}
+
+func cleanDataTransportDir(ctx context.Context, filepath string) error {
+	framework.LogWithContext(ctx).Infof("clean and re-mkdir data dir: %s", filepath)
+	if err := os.RemoveAll(filepath); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(filepath, os.ModeDir); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateTransportRecordFailed(ctx context.Context, recordId string, clusterId string) error {
+	rw := models.GetImportExportReaderWriter()
+	err := rw.UpdateDataTransportRecord(ctx, recordId, string(constants.DataImportExportFailed), time.Now())
+	if err != nil {
+		framework.LogWithContext(ctx).Errorf("update data transport record failed, %s", err.Error())
+		return err
+	}
+
+	framework.LogWithContext(ctx).Info("update data transport record success")
+	return nil
+}

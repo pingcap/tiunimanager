ed7ffde2df7e19611884558d5389a663c50dad2b
chencheng <chencheng@pingcap.com>
2021-12-10 18:10:01 +0800

    modify file-server query records

--- file-server/controller/common.go
+++ file-server/controller/common.go
@@ -6,12 +6,11 @@
  *                                                                            *
  * http://www.apache.org/licenses/LICENSE-2.0                                 *
  *                                                                            *
- * Unless required by applicable law or agreed to in writing, software        *
- * distributed under the License is distributed on an "AS IS" BASIS,          *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- * See the License for the specific language governing permissions and        *
- * limitations under the License.                                             *
- *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
  ******************************************************************************/
 
 // Package classification TiEM API.
@@ -34,33 +33,136 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/pingcap-inc/tiem/library/client/cluster/clusterpb"
+
 	"github.com/asim/go-micro/v3/client"
 	"github.com/gin-gonic/gin"
+	"github.com/pingcap-inc/tiem/file-server/interceptor"
+	"github.com/pingcap-inc/tiem/library/knowledge"
 )
 
-type ResultMark struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
-type CommonFileResult struct {
-	ResultMark
-	Data interface{} `json:"data"`
-}
-
+// DefaultTimeout
+// todo adjust timeout for async flow task
 var DefaultTimeout = func(o *client.CallOptions) {
-	o.RequestTimeout = time.Second * 30
-	o.DialTimeout = time.Second * 30
+	o.RequestTimeout = time.Minute * 5
+	o.DialTimeout = time.Minute * 5
 }
 
-func Success(data interface{}) *CommonFileResult {
-	return &CommonFileResult{ResultMark: ResultMark{0, "OK"}, Data: data}
+type Usage struct {
+	Total     float32 `json:"total"`
+	Used      float32 `json:"used"`
+	UsageRate float32 `json:"usageRate"`
 }
 
-func Fail(code int, message string) *CommonFileResult {
-	return &CommonFileResult{ResultMark{code, message}, struct{}{}}
+type Page struct {
+	Page     int `json:"page"`
+	PageSize int `json:"pageSize"`
+	Total    int `json:"total"`
+}
+
+var DefaultPageRequest = PageRequest{
+	1,
+	20,
+}
+
+type PageRequest struct {
+	Page     int `json:"page" form:"page"`
+	PageSize int `json:"pageSize" form:"pageSize"`
 }
 
 func Hello(c *gin.Context) {
 	c.JSON(http.StatusOK, Success("hello"))
+}
+
+func HelloPage(c *gin.Context) {
+	c.JSON(http.StatusOK, Success("hello world"))
+}
+
+type StatusInfo struct {
+	StatusCode      string    `json:"statusCode"`
+	StatusName      string    `json:"statusName"`
+	InProcessFlowId int       `json:"inProcessFlowId"`
+	CreateTime      time.Time `json:"createTime"`
+	UpdateTime      time.Time `json:"updateTime"`
+	DeleteTime      time.Time `json:"deleteTime"`
+}
+
+type Operator struct {
+	ManualOperator bool   `json:"manualOperator"`
+	OperatorName   string `json:"operatorName"`
+	OperatorId     string `json:"operatorId"`
+	TenantId       string `json:"tenantId"`
+}
+
+func GetOperator(c *gin.Context) *Operator {
+	v, _ := c.Get(interceptor.VisitorIdentityKey)
+
+	visitor, _ := v.(*interceptor.VisitorIdentity)
+
+	return &Operator{
+		ManualOperator: true,
+		OperatorId:     visitor.AccountId,
+		OperatorName:   visitor.AccountName,
+		TenantId:       visitor.TenantId,
+	}
+}
+
+func (o *Operator) ConvertToDTO() *clusterpb.OperatorDTO {
+	return &clusterpb.OperatorDTO{
+		Id:       o.OperatorId,
+		Name:     o.OperatorName,
+		TenantId: o.TenantId,
+	}
+}
+
+func (p *PageRequest) ConvertToDTO() *clusterpb.PageDTO {
+	return &clusterpb.PageDTO{
+		Page:     int32(p.Page),
+		PageSize: int32(p.PageSize),
+	}
+}
+
+func ParsePageFromDTO(dto *clusterpb.PageDTO) *Page {
+	return &Page{
+		Page:     int(dto.Page),
+		PageSize: int(dto.PageSize),
+		Total:    int(dto.Total),
+	}
+}
+
+func ParseUsageFromDTO(dto *clusterpb.UsageDTO) (usage *Usage) {
+	usage = &Usage{
+		Total:     dto.Total,
+		Used:      dto.Used,
+		UsageRate: dto.UsageRate,
+	}
+	return
+}
+
+func ConvertVersionDTO(code string) (dto *clusterpb.ClusterVersionDTO) {
+	version := knowledge.ClusterVersionFromCode(code)
+
+	dto = &clusterpb.ClusterVersionDTO{
+		Code: version.Code,
+		Name: version.Name,
+	}
+
+	return
+}
+
+func ConvertTypeDTO(code string) (dto *clusterpb.ClusterTypeDTO) {
+	t := knowledge.ClusterTypeFromCode(code)
+
+	dto = &clusterpb.ClusterTypeDTO{
+		Code: t.Code,
+		Name: t.Name,
+	}
+	return
+}
+
+func ConvertRecoverInfoDTO(sourceClusterId string, backupRecordId int64) (dto *clusterpb.RecoverInfoDTO) {
+	return &clusterpb.RecoverInfoDTO{
+		SourceClusterId: sourceClusterId,
+		BackupRecordId:  backupRecordId,
+	}
 }
---
+++ file-server/controller/httprequest.go
@@ -0,0 +1,127 @@
+/******************************************************************************
+ * Copyright (c)  2021 PingCAP, Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+package controller
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"github.com/pingcap-inc/tiem/library/framework"
+	"net/http"
+)
+
+// HandleJsonRequestWithBuiltReq
+// @Description: handle common json request with a built request
+// validate request using validator.New().Struct(req)
+// serialize request using json.Marshal(req)
+// @Parameter c
+// @Parameter req
+// @Parameter appenders append request data out of http body, such as id in path
+// @return err
+// @return requestBody
+func HandleJsonRequestWithBuiltReq(c *gin.Context, req interface{}) (requestBody string, err error) {
+	return HandleRequest(c,
+		&req,
+		func(c *gin.Context, req interface{}) error {
+			return nil
+		},
+		func(req interface{}) error {
+			return validator.New().Struct(req)
+		},
+		func(req interface{}) ([]byte, error) {
+			return json.Marshal(req)
+		},
+	)
+}
+
+// HandleJsonRequestFromBody
+// @Description: handle common json request from body
+// build request using c.ShouldBindBodyWith(req, binding.JSON)
+// validate request using validator.New().Struct(req)
+// serialize request using json.Marshal(req)
+// @Parameter c
+// @Parameter req
+// @Parameter appenders append request data out of http body, such as id in path
+// @return err
+// @return requestBody
+func HandleJsonRequestFromBody(c *gin.Context,
+	req interface{},
+	appenders ...func(c *gin.Context, req interface{}) error,
+) (requestBody string, err error) {
+	return HandleRequest(c,
+		&req,
+		func(c *gin.Context, req interface{}) error {
+			err = c.ShouldBindBodyWith(req, binding.JSON)
+			if err != nil {
+				return err
+			}
+			for _, appender := range appenders {
+				err = appender(c, req)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+		func(req interface{}) error {
+			return validator.New().Struct(req)
+		},
+		func(req interface{}) ([]byte, error) {
+			return json.Marshal(req)
+		},
+	)
+}
+
+// HandleRequest
+// @Description: handle request
+// @Parameter c
+// @Parameter req
+// @Parameter builder build request from gin.Context
+// @Parameter validator validate request
+// @Parameter serializer serialize request to string
+// @return err
+// @return requestBody
+func HandleRequest(c *gin.Context,
+	req interface{},
+	builder func(c *gin.Context, req interface{}) error,
+	validator func(req interface{}) error,
+	serializer func(req interface{}) ([]byte, error)) (requestBody string, err error) {
+
+	err = builder(c, req)
+	if err != nil {
+		framework.LogWithContext(c).Errorf("get request failed, %s", err.Error())
+		c.JSON(http.StatusBadRequest, Fail(http.StatusBadRequest, err.Error()))
+		return
+	}
+
+	err = validator(req)
+	if err != nil {
+		framework.LogWithContext(c).Errorf("validate request failed, %s", err.Error())
+		c.JSON(http.StatusBadRequest, Fail(http.StatusBadRequest, err.Error()))
+		return
+	}
+
+	requestBodyBytes, err := serializer(req)
+	if err != nil {
+		framework.LogWithContext(c).Errorf("validate request failed, %s", err.Error())
+		c.JSON(http.StatusBadRequest, Fail(http.StatusBadRequest, err.Error()))
+		return
+	} else {
+		requestBody = string(requestBodyBytes)
+		return
+	}
+}

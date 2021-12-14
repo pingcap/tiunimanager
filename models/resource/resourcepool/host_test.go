f8ed213c3f0d75e7882550824820431250ef9b05
Jarvis Zheng <jiayang@hust.edu.cn>
2021-12-14 11:37:29 +0800

    [Resource Manager] Add UT cases for host models

--- models/resource/resourcepool/host_test.go
+++ models/resource/resourcepool/host_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/pingcap-inc/tiem/common/constants"
+	"github.com/pingcap-inc/tiem/common/structs"
 	"github.com/pingcap-inc/tiem/library/common"
 	"github.com/pingcap-inc/tiem/library/framework"
 	"github.com/pingcap-inc/tiem/library/util/uuidutil"
@@ -115,6 +116,7 @@ func createDB(path string) (db *gorm.DB, err error) {
 
 func genFakeHost(region, zone, rack, hostName, ip string, freeCpuCores, freeMemory int32, clusterType, purpose, diskType string) (h *Host) {
 	host := Host{
+		ID:           "xxxx-aaaa-bbbb-cccc",
 		HostName:     hostName,
 		IP:           ip,
 		UserName:     "root",
@@ -167,4 +169,38 @@ func Test_Host_Hooks(t *testing.T) {
 	err = db.Unscoped().Where("id = ?", host.ID).Find(&queryHost).Error
 	assert.Nil(t, err)
 	assert.Equal(t, string(constants.HostDeleted), queryHost.Status)
+}
+
+func Test_ConstructFromHostInfo(t *testing.T) {
+	src := structs.HostInfo{
+		IP:       "192.168.999.999",
+		UserName: "root",
+		Region:   "TEST_Region1",
+		AZ:       "TEST_Zone1",
+		Rack:     "TEST_Rack1",
+		Disks: []structs.DiskInfo{
+			{Name: "sda", Path: "/", Capacity: 256, Status: string(constants.DiskReserved), Type: string(constants.SSD)},
+			{Name: "sdb", Path: "/", Capacity: 256, Status: string(constants.DiskAvailable), Type: string(constants.NVMeSSD)},
+		},
+	}
+	dst := &Host{}
+	dst.ConstructFromHostInfo(&src)
+	assert.Equal(t, "192.168.999.999", dst.IP)
+	assert.Equal(t, "root", dst.UserName)
+	assert.Equal(t, "TEST_Region1", dst.Region)
+	assert.Equal(t, "TEST_Region1,TEST_Zone1", dst.AZ)
+	assert.Equal(t, "TEST_Region1,TEST_Zone1,TEST_Rack1", dst.Rack)
+	assert.Equal(t, 2, len(dst.Disks))
+}
+
+func Test_ToHostInfo(t *testing.T) {
+	src := genFakeHost("Region1", "Region1,Zone1", "Region1,Zone1,Rack1", "TEST_HOST1", "192.168.999.999", 32, 64,
+		string(constants.EMProductNameDataMigration), string(constants.PurposeSchedule), string(constants.NVMeSSD))
+
+	var dst structs.HostInfo
+	src.ToHostInfo(&dst)
+	assert.Equal(t, "Region1", dst.Region)
+	assert.Equal(t, "Zone1", dst.AZ)
+	assert.Equal(t, "Rack1", dst.Rack)
+	assert.Equal(t, 2, len(dst.Disks))
 }

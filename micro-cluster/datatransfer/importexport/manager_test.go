5a0a73c8f5c9ab4c075318c8838de6da376615e9
chencheng <chencheng@pingcap.com>
2021-12-27 18:58:18 +0800

    add test case

--- micro-cluster/datatransfer/importexport/manager_test.go
+++ micro-cluster/datatransfer/importexport/manager_test.go
@@ -18,11 +18,19 @@ package importexport
 import (
 	"context"
 	"github.com/golang/mock/gomock"
+	"github.com/pingcap-inc/tiem/common/constants"
 	"github.com/pingcap-inc/tiem/message"
 	"github.com/pingcap-inc/tiem/models"
+	"github.com/pingcap-inc/tiem/models/cluster/management"
 	"github.com/pingcap-inc/tiem/models/common"
 	"github.com/pingcap-inc/tiem/models/datatransfer/importexport"
+	"github.com/pingcap-inc/tiem/models/platform/config"
+	wfModel "github.com/pingcap-inc/tiem/models/workflow"
+	"github.com/pingcap-inc/tiem/test/mockmodels/mockclustermanagement"
+	"github.com/pingcap-inc/tiem/test/mockmodels/mockconfig"
 	"github.com/pingcap-inc/tiem/test/mockmodels/mockimportexport"
+	mock_workflow_service "github.com/pingcap-inc/tiem/test/mockworkflow"
+	"github.com/pingcap-inc/tiem/workflow"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -34,6 +42,116 @@ func init() {
 func TestGetImportExportService(t *testing.T) {
 	service := GetImportExportService()
 	assert.NotNil(t, service)
+}
+
+func TestImportExportManager_ExportData(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	clusterRW := mockclustermanagement.NewMockReaderWriter(ctrl)
+	models.SetClusterReaderWriter(clusterRW)
+	clusterRW.EXPECT().GetMeta(gomock.Any(), gomock.Any()).Return(&management.Cluster{
+		Entity: common.Entity{
+			ID:       "id-xxxx",
+			TenantId: "tid-xxx",
+		},
+	}, make([]*management.ClusterInstance, 0), nil).AnyTimes()
+	clusterRW.EXPECT().SetMaintenanceStatus(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+	workflowService := mock_workflow_service.NewMockWorkFlowService(ctrl)
+	workflow.MockWorkFlowService(workflowService)
+	defer workflow.MockWorkFlowService(workflow.NewWorkFlowManager())
+	workflowService.EXPECT().RegisterWorkFlow(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+	workflowService.EXPECT().CreateWorkFlow(gomock.Any(), gomock.Any(), gomock.Any()).Return(&workflow.WorkFlowAggregation{
+		Flow:    &wfModel.WorkFlow{Entity: common.Entity{ID: "flow01"}},
+		Context: workflow.FlowContext{Context: context.TODO(), FlowData: make(map[string]interface{})},
+	}, nil).AnyTimes()
+	workflowService.EXPECT().AddContext(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+	workflowService.EXPECT().AsyncStart(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+	configService := mockconfig.NewMockReaderWriter(ctrl)
+	configService.EXPECT().GetConfig(gomock.Any(), gomock.Any()).Return(&config.SystemConfig{ConfigValue: string(constants.StorageTypeNFS)}, nil).AnyTimes()
+	models.SetConfigReaderWriter(configService)
+
+	transportService := mockimportexport.NewMockReaderWriter(ctrl)
+	transportService.EXPECT().CreateDataTransportRecord(gomock.Any(), gomock.Any()).Return(&importexport.DataTransportRecord{Entity: common.Entity{
+		ID: "xxx",
+	}}, nil).AnyTimes()
+	models.SetImportExportReaderWriter(transportService)
+
+	service := GetImportExportService()
+	resp, err := service.ExportData(context.TODO(), message.DataExportReq{
+		ClusterID:       "test-cls",
+		UserName:        "userName",
+		Password:        "password",
+		FileType:        "csv",
+		Filter:          "filter",
+		StorageType:     string(constants.StorageTypeS3),
+		ZipName:         "export.zip",
+		EndpointUrl:     "endpointUrl",
+		BucketUrl:       "bucketUrl",
+		AccessKey:       "ak",
+		SecretAccessKey: "sk",
+		Comment:         "comment",
+	})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, resp.WorkFlowID)
+}
+
+func TestImportExportManager_ImportData(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	clusterRW := mockclustermanagement.NewMockReaderWriter(ctrl)
+	models.SetClusterReaderWriter(clusterRW)
+	clusterRW.EXPECT().GetMeta(gomock.Any(), gomock.Any()).Return(&management.Cluster{
+		Entity: common.Entity{
+			ID:       "id-xxxx",
+			TenantId: "tid-xxx",
+		},
+	}, make([]*management.ClusterInstance, 0), nil).AnyTimes()
+	clusterRW.EXPECT().SetMaintenanceStatus(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+	workflowService := mock_workflow_service.NewMockWorkFlowService(ctrl)
+	workflow.MockWorkFlowService(workflowService)
+	defer workflow.MockWorkFlowService(workflow.NewWorkFlowManager())
+	workflowService.EXPECT().RegisterWorkFlow(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+	workflowService.EXPECT().CreateWorkFlow(gomock.Any(), gomock.Any(), gomock.Any()).Return(&workflow.WorkFlowAggregation{
+		Flow:    &wfModel.WorkFlow{Entity: common.Entity{ID: "flow01"}},
+		Context: workflow.FlowContext{Context: context.TODO(), FlowData: make(map[string]interface{})},
+	}, nil).AnyTimes()
+	workflowService.EXPECT().AddContext(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+	workflowService.EXPECT().AsyncStart(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+	configService := mockconfig.NewMockReaderWriter(ctrl)
+	configService.EXPECT().GetConfig(gomock.Any(), gomock.Any()).Return(&config.SystemConfig{ConfigValue: string(constants.StorageTypeNFS)}, nil).AnyTimes()
+	models.SetConfigReaderWriter(configService)
+
+	transportService := mockimportexport.NewMockReaderWriter(ctrl)
+	transportService.EXPECT().CreateDataTransportRecord(gomock.Any(), gomock.Any()).Return(&importexport.DataTransportRecord{Entity: common.Entity{
+		ID: "xxx",
+	}}, nil).AnyTimes()
+	models.SetImportExportReaderWriter(transportService)
+
+	service := GetImportExportService()
+	resp, err := service.ExportData(context.TODO(), message.DataExportReq{
+		ClusterID:       "test-cls",
+		UserName:        "userName",
+		Password:        "password",
+		FileType:        "csv",
+		Filter:          "filter",
+		StorageType:     string(constants.StorageTypeS3),
+		ZipName:         "export.zip",
+		EndpointUrl:     "endpointUrl",
+		BucketUrl:       "bucketUrl",
+		AccessKey:       "ak",
+		SecretAccessKey: "sk",
+		Comment:         "comment",
+	})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, resp.WorkFlowID)
 }
 
 func TestImportExportManager_DeleteDataTransportRecord(t *testing.T) {

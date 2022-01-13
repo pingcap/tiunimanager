fbeb0d6ed3b00917ba6811efaf34bada804f4a85
Sen Han <00hnes@gmail.com>
2022-01-13 14:15:31 +0800

    switchover: draft

--- micro-cluster/cluster/switchover/check_db_writable.go
+++ micro-cluster/cluster/switchover/check_db_writable.go
@@ -40,6 +40,52 @@ func (c CheckWritable) TableName() string {
 	return fmt.Sprintf("%s_%s", tableNamePrefix, c.tableNameSuffix)
 }
 
+// set cluster Read-Writeable to normal user and changeFeedTask's user
+func (p *Manager) clusterRestrictedReadOnlyOp(ctx context.Context, clusterID, addr, op string, setReadOnlyFlag bool) (readOnlyFlag bool, err error) {
+	funcName := "clusterRestrictedReadOnlyOp"
+	framework.LogWithContext(ctx).Infof("start %s", funcName)
+	defer framework.LogWithContext(ctx).Infof("exit %s", funcName)
+	if op == "set" || op == "get" {
+	} else {
+		framework.LogWithContext(ctx).Fatalf(
+			"%s clusterRestrictedReadOnlyOp clusterID:%s unknow op:%s", funcName, clusterID, op)
+	}
+	name, pwd, err := mgr.clusterGetCDCUserNameAndPwd(ctx, clusterID)
+	if err != nil {
+		framework.LogWithContext(ctx).Warnf(
+			"%s clusterGetCDCUserNameAndPwd clusterID:%s err:%s", funcName, clusterID, err)
+		return readOnlyFlag, err
+	} else {
+		framework.LogWithContext(ctx).Debugf(
+			"%s clusterGetCDCUserNameAndPwd clusterID:%s success", funcName, clusterID)
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/?charset=utf8mb4&parseTime=True&loc=Local", name, pwd, addr)
+	mylog := framework.LogWithContext(ctx).WithField("clusterID", clusterID)
+	mylog.Info("checkClusterWritable gorm.Open with dsn:", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		mylog.Warnf("%s gorm.Open err:", funcName, err)
+		return readOnlyFlag, err
+	} else {
+		mylog.Debugf("%s gorm.Open success", funcName)
+	}
+	if op == "set" {
+		value := 0
+		if setReadOnlyFlag {
+			value = 1
+		}
+		err = db.Exec(fmt.Sprintf("SET GLOBAL tidb_restricted_read_only = %d ;", value)).Error
+		if err != nil {
+			return false, err
+		} else {
+			return setReadOnlyFlag, err
+		}
+	} else {
+
+	}
+	return readOnlyFlag, nil
+}
+
 func (p *Manager) checkClusterWritable(ctx context.Context, clusterID, userName, pwd, addr string) error {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", userName, pwd, addr, databaseName)
 	mylog := framework.LogWithContext(ctx).WithField("clusterID", clusterID)

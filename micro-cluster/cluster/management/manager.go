d27844ba3331d9d7c57631127d33523535980a01
zhangpeijin-milo <zhangpeijin@pingcap.com>
2022-01-22 19:12:40 +0800

    [scaleout]fix

--- micro-cluster/cluster/management/manager.go
+++ micro-cluster/cluster/management/manager.go
@@ -417,14 +417,6 @@ func (p *Manager) PreviewScaleOutCluster(ctx context.Context, req cluster.ScaleO
 		return
 	}
 
-	// When scale out TiFlash, Judge whether enable-placement-rules is true
-	err = handler.ScaleOutPreCheck(ctx, clusterMeta, req.InstanceResource)
-	if err != nil {
-		framework.LogWithContext(ctx).Errorf(
-			"check cluster %s scale out error: %s", clusterMeta.Cluster.ID, err.Error())
-		return
-	}
-
 	// todo validate
 	resp = cluster.PreviewClusterResp{
 		Region:            clusterMeta.Cluster.Region,

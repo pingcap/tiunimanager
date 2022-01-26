9ce8bfa7753e4a097eb627f918dd85402635c8cf
zhangpeijin-milo <zhangpeijin@pingcap.com>
2022-01-26 14:59:09 +0800

    [user]index

--- models/cluster/management/clusteruser.go
+++ models/cluster/management/clusteruser.go
@@ -23,7 +23,7 @@ import (
 type DBUser struct {
 	gorm.Model
 	ClusterID                string `gorm:"not null;type:varchar(22);default:null"`
-	Name                     string `gorm:"default:null;not null;uniqueIndex;comment:'name of the user'"`
+	Name                     string `gorm:"default:null;not null;comment:'name of the user'"`
 	Password                 string `gorm:"not null;size:64;comment:'password of the user'"`
 	RoleType                 string `gorm:"not null;size:64;comment:'role type of the user'"`
 	LastPasswordGenerateTime time.Time

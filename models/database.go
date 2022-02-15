85157efb9bf3bba3f8aed64cd9f62f078aaeb8bd
chencheng <chencheng@pingcap.com>
2022-02-15 16:41:02 +0800

    fix bug

--- models/database.go
+++ models/database.go
@@ -183,7 +183,10 @@ func (p *database) initReaderWriters() {
 
 func (p *database) initSystemConfig() {
 	// system config
-	defaultDb.configReaderWriter.CreateConfig(context.TODO(), &config.SystemConfig{ConfigKey: constants.ConfigKeyBackupStorageType, ConfigValue: string(constants.StorageTypeS3)})
+	_, err := defaultDb.configReaderWriter.CreateConfig(context.TODO(), &config.SystemConfig{ConfigKey: constants.ConfigKeyBackupStorageType, ConfigValue: string(constants.StorageTypeS3)})
+	if err != nil {
+		framework.LogForkFile(constants.LogFileSystem).Errorf("ERRRRRR: %s", err.Error())
+	}
 	defaultDb.configReaderWriter.CreateConfig(context.TODO(), &config.SystemConfig{ConfigKey: constants.ConfigKeyBackupStoragePath, ConfigValue: constants.DefaultBackupStoragePath})
 	defaultDb.configReaderWriter.CreateConfig(context.TODO(), &config.SystemConfig{ConfigKey: constants.ConfigKeyBackupS3AccessKey, ConfigValue: constants.DefaultBackupS3AccessKey})
 	defaultDb.configReaderWriter.CreateConfig(context.TODO(), &config.SystemConfig{ConfigKey: constants.ConfigKeyBackupS3SecretAccessKey, ConfigValue: constants.DefaultBackupS3SecretAccessKey})

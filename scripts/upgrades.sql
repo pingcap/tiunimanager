193151613436636793f50260b4d610dfa37d780c
Haibo Shen <haibo@umich.edu>
2022-02-18 01:27:27 +0800

    upgrade: fix sql

--- scripts/upgrades.sql
+++ scripts/upgrades.sql
@@ -1,30 +1,30 @@
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.0', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.1', datetime('now'), datetime('now'));
+VALUES ('2', 'in-place', 'TiDB', 'v4.0', 'v5.1', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.2', datetime('now'), datetime('now'));
+VALUES ('3', 'in-place', 'TiDB', 'v4.0', 'v5.2', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.3', datetime('now'), datetime('now'));
+VALUES ('4', 'in-place', 'TiDB', 'v4.0', 'v5.3', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.4', datetime('now'), datetime('now'));
+VALUES ('5', 'in-place', 'TiDB', 'v4.0', 'v5.4', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.1', datetime('now'), datetime('now'));
+VALUES ('6', 'in-place', 'TiDB', 'v5.0', 'v5.1', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.2', datetime('now'), datetime('now'));
+VALUES ('7', 'in-place', 'TiDB', 'v5.0', 'v5.2', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.3', datetime('now'), datetime('now'));
+VALUES ('8', 'in-place', 'TiDB', 'v5.0', 'v5.3', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.4', datetime('now'), datetime('now'));
+VALUES ('9', 'in-place', 'TiDB', 'v5.0', 'v5.4', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.1', 'v5.2', datetime('now'), datetime('now'));
+VALUES ('10', 'in-place', 'TiDB', 'v5.1', 'v5.2', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.1', 'v5.3', datetime('now'), datetime('now'));
+VALUES ('11', 'in-place', 'TiDB', 'v5.1', 'v5.3', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.1', 'v5.4', datetime('now'), datetime('now'));
+VALUES ('12', 'in-place', 'TiDB', 'v5.1', 'v5.4', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.2', 'v5.3', datetime('now'), datetime('now'));
+VALUES ('13', 'in-place', 'TiDB', 'v5.2', 'v5.3', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.2', 'v5.4', datetime('now'), datetime('now'));
+VALUES ('14', 'in-place', 'TiDB', 'v5.2', 'v5.4', datetime('now'), datetime('now'));
 INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
-VALUES ('1', 'in-place', 'TiDB', 'v5.3', 'v5.4', datetime('now'), datetime('now'));
+VALUES ('15', 'in-place', 'TiDB', 'v5.3', 'v5.4', datetime('now'), datetime('now'));

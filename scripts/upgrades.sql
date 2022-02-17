a79611d333de3fa5e574915bd3ddc75603ab901e
Haibo Shen <haibo@umich.edu>
2022-02-18 01:17:37 +0800

    upgrade: fix sql

--- scripts/upgrades.sql
+++ scripts/upgrades.sql
@@ -1,30 +1,30 @@
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.0', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.1', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.2', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.3', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v4.0', 'v5.4', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.1', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.2', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.3', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.0', 'v5.4', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.1', 'v5.2', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.1', 'v5.3', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.1', 'v5.4', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.2', 'v5.3', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.2', 'v5.4', datetime('now'), datetime('now'));
-INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, create_at, update_at)
+INSERT INTO product_upgrade_paths (id, type, product_id, src_version, dst_version, created_at, updated_at)
 VALUES ('1', 'in-place', 'TiDB', 'v5.3', 'v5.4', datetime('now'), datetime('now'));

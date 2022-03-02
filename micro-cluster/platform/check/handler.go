001cae2a95feaebe6c1beff0ce398fa43da2d819
yaozheng.wyz <wangyaozheng@pingcap.com>
2022-03-02 12:14:31 +0800

    Check: fix bug for ut

--- micro-cluster/platform/check/handler.go
+++ micro-cluster/platform/check/handler.go
@@ -13,7 +13,7 @@
  * limitations under the License.                                             *
  ******************************************************************************/
 
-package handler
+package check
 
 import (
 	"context"

388a4a950c56c7f589af731ce69a39c551b72af1
zhangpeijin-milo <zhangpeijin@pingcap.com>
2022-03-10 20:37:29 +0800

    [product]refactor validation

--- micro-cluster/cluster/management/validator.go
+++ micro-cluster/cluster/management/validator.go
@@ -19,75 +19,60 @@ import (
 	"context"
 	"github.com/pingcap-inc/tiem/common/constants"
 	"github.com/pingcap-inc/tiem/common/errors"
-	"github.com/pingcap-inc/tiem/common/structs"
 	"github.com/pingcap-inc/tiem/library/framework"
 	"github.com/pingcap-inc/tiem/message/cluster"
-	"github.com/pingcap-inc/tiem/micro-cluster/cluster/management/meta"
+	"github.com/pingcap-inc/tiem/models"
 )
 
-var creatingRules = []func(req *cluster.CreateClusterReq, product *structs.ProductDetail) error{
-	func(req *cluster.CreateClusterReq, product *structs.ProductDetail) error {
-		if _, ok := product.Versions[req.Version]; !ok {
-			return errors.NewErrorf(errors.TIEM_UNSUPPORT_PRODUCT, "version %s is not supported", req.Version)
+var validator = validateCreating
+
+func validateCreating(ctx context.Context, req *cluster.CreateClusterReq) error {
+	product, versions, components, err := models.GetProductReaderWriter().GetProduct(ctx, req.Type)
+	if err != nil {
+		return errors.WrapError(errors.TIEM_UNSUPPORT_PRODUCT, "get product failed", err)
+	}
+
+	return errors.OfNullable(nil).BreakIf(func() error {
+		if len(product.ProductID) == 0 || len(versions) == 0 || len(components) == 0 {
+			return errors.NewErrorf(errors.TIEM_UNSUPPORT_PRODUCT, "product %s is not available", req.Type)
 		}
 		return nil
-	},
-	func(req *cluster.CreateClusterReq, product *structs.ProductDetail) error {
-		if _, ok := product.Versions[req.Version].Arch[req.CpuArchitecture]; !ok {
-			return errors.NewErrorf(errors.TIEM_UNSUPPORT_PRODUCT, "arch %s is not supported", req.CpuArchitecture)
+	}).BreakIf(func() error {
+		for _, v := range versions {
+			if v.ProductID == req.Type && v.Version == req.Version && v.Arch == req.CpuArchitecture {
+				return nil
+			}
 		}
-		return nil
-	},
-	func(req *cluster.CreateClusterReq, product *structs.ProductDetail) error {
-		// TiKV
+		return errors.NewErrorf(errors.TIEM_UNSUPPORT_PRODUCT, "product is not available, productID = %s, version = %s, arch = %s", req.Type, req.Version, req.CpuArchitecture)
+	}).BreakIf(func() error {
 		count := req.ResourceParameter.GetComponentCount(constants.ComponentIDTiKV)
 		if count < int32(req.Copies) {
 			return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "count of TiKV = %d, is less than copies %d", count, req.Copies)
 		}
 		return nil
-	},
-	func(req *cluster.CreateClusterReq, product *structs.ProductDetail) error {
-		for _, property := range product.Versions[req.Version].Arch[req.CpuArchitecture] {
-			count := req.ResourceParameter.GetComponentCount(constants.EMProductComponentIDType(property.ID))
-			if count > property.MaxInstance {
-				return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "count of components %s should be less than %d", property.ID, property.MaxInstance)
+	}).BreakIf(func() error {
+		for _, componentInfo := range components {
+			count := req.ResourceParameter.GetComponentCount(constants.EMProductComponentIDType(componentInfo.ComponentID))
+			if count > componentInfo.MaxInstance {
+				return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "count of components %s should be less than %d", componentInfo.ComponentID, componentInfo.MaxInstance)
 			}
-			if count < property.MinInstance {
-				return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "count of components %s should be more than %d", property.ID, property.MinInstance)
+			if count < componentInfo.MinInstance {
+				return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "count of components %s should be more than %d", componentInfo.ComponentID, componentInfo.MinInstance)
 			}
-			if len(property.SuggestedInstancesCount) > 0 {
+			if len(componentInfo.SuggestedInstancesCount) > 0 {
 				matched := false
-				for _, c := range property.SuggestedInstancesCount {
+				for _, c := range componentInfo.SuggestedInstancesCount {
 					if c == count {
 						matched = true
 					}
 				}
 				if !matched {
-					return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "the total number of %s should be in %v", property.ID, property.SuggestedInstancesCount)
+					return errors.NewErrorf(errors.TIEM_INVALID_TOPOLOGY, "the total number of %s should be in %v", componentInfo.ComponentID, componentInfo.SuggestedInstancesCount)
 				}
 			}
 		}
 		return nil
-	},
-}
-
-var validator = validateCreating
-
-func validateCreating(ctx context.Context, req *cluster.CreateClusterReq) error {
-	optionalError := errors.OfNullable(nil)
-
-	product, err := meta.GetProductDetail(ctx, req.Vendor, req.Region, req.Type)
-	if err != nil {
-		return err
-	} else {
-		for _, v := range creatingRules {
-			optionalError.BreakIf(func() error {
-				return v(req, product)
-			})
-		}
-	}
-
-	return optionalError.If(func(err error) {
+	}).If(func(err error) {
 		framework.LogWithContext(ctx).Error(err.Error())
 	}).Else(func() {
 		framework.LogWithContext(ctx).Infof("validate creating request succeed")

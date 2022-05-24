df74b5d21fe3ebf6fb968cf744f6be4a32cef5d1
Haibo Shen <haibo@umich.edu>
2022-05-24 14:43:55 +0800

    modify quick_run

--- quick_run.sh
+++ quick_run.sh
@@ -1,4 +1,4 @@
-nohup ./bin/cluster-server --host=127.0.0.1 --port=4101 --tracer-address=127.0.0.1:6831 --metrics-port=4104 --registry-address=127.0.0.1:4106 --elasticsearch-address=127.0.0.1:9200 --skip-host-init=true  --em-version=InTesting >> logs/system.log 2>&1 &
+nohup ./bin/cluster-server --host=127.0.0.1 --port=4101 --tracer-address=127.0.0.1:6831 --metrics-port=4104 --registry-address=127.0.0.1:4106 --elasticsearch-address=127.0.0.1:9200 --skip-host-init=true  --em-version=InTesting --deploy-user=tidb --deploy-group=tidb >> logs/system.log 2>&1 &
 sleep 3
 nohup ./bin/openapi-server --host=127.0.0.1 --port=4116 --tracer-address=127.0.0.1:6831 --metrics-port=4103 --registry-address=127.0.0.1:4106 >> system.log &
 sleep 3

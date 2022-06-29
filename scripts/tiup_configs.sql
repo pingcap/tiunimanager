2eb2d32e1372befe928f5e990bdd6c9617cee402
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 23:04:24 +0800

    Delete scripts directory

--- scripts/tiup_configs.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO system_configs (id, created_at, updated_at, config_key, config_value)
-VALUES ('default', datetime('now'), datetime('now'), 'default_tiup_home', '/home/tidb/.tiup');
-INSERT INTO system_configs (id, created_at, updated_at, config_key, config_value)
-VALUES ('em', datetime('now'), datetime('now'), 'em_tiup_home', '/home/tidb/.em');

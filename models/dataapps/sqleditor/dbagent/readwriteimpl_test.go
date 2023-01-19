ace65f6b85dff4efd9ca8a86974d33e23b897a48
Riven Wang <jiangtao.wang@pingcap.com>
2023-01-19 23:17:29 +0800

    code cover

--- models/dataapps/sqleditor/dbagent/readwriteimpl_test.go
+++ models/dataapps/sqleditor/dbagent/readwriteimpl_test.go
@@ -1,6 +1,7 @@
 package dbagent
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
@@ -23,17 +24,34 @@ func TestDbagent_GetTableMetaData(t *testing.T) {
 	dbagent := DBAgent{
 		DB: db,
 	}
-	dbName := "test"
-	tableName := "test"
-	clusterID := "test"
-	columns := []string{"col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8"}
-	rows := sqlmock.NewRows(columns).AddRow("value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8")
-	rows = rows.AddRow("value12", "value22", "value32", "value42", "value52", "value62", "value72", "value82")
-	mock.ExpectQuery(fmt.Sprintf(tableMetaQuery, dbName, tableName)).WillReturnRows(rows)
-	res, err := dbagent.GetTableMetaData(c, clusterID, dbName, tableName)
-	assert.Equal(t, len(res.Columns), 2)
-	assert.Equal(t, res.Columns[0].Col, "value4")
-	assert.NoError(t, err)
+	t.Run("suc", func(t *testing.T) {
+		dbName := "test"
+		tableName := "test"
+		clusterID := "test"
+		columns := []string{"col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8"}
+		rows := sqlmock.NewRows(columns).AddRow("value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8")
+		rows = rows.AddRow("value12", "value22", "value32", "value42", "value52", "value62", "value72", "value82")
+		mock.ExpectQuery(fmt.Sprintf(tableMetaQuery, dbName, tableName)).WillReturnRows(rows)
+		res, err := dbagent.GetTableMetaData(c, clusterID, dbName, tableName)
+		assert.Equal(t, len(res.Columns), 2)
+		assert.Equal(t, res.Columns[0].Col, "value4")
+		assert.NoError(t, err)
+
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		dbName := "test"
+		tableName := "test"
+		clusterID := "test"
+		columns := []string{"col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8"}
+		rows := sqlmock.NewRows(columns).AddRow("value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8")
+		rows = rows.AddRow("value12", "value22", "value32", "value42", "value52", "value62", "value72", "value82")
+		mock.ExpectQuery(fmt.Sprintf(tableMetaQuery, dbName, tableName)).WillReturnError(errors.New("failed"))
+		_, err := dbagent.GetTableMetaData(c, clusterID, dbName, tableName)
+		assert.Error(t, err)
+
+	})
+
 }
 
 func TestDbagent_GetClusterMetaData(t *testing.T) {
@@ -78,6 +96,29 @@ func TestDbagent_GetClusterMetaData(t *testing.T) {
 		res, err := dbagent.GetClusterMetaData(c, false, true)
 		assert.Equal(t, res[0].Name, "INFORMATION_SCHEMA")
 		assert.NoError(t, err)
+	})
+
+	t.Run("get table failed", func(t *testing.T) {
+		columns := []string{"schema_name", "t1"}
+		rows := sqlmock.NewRows(columns).AddRow("INFORMATION_SCHEMA", "1")
+		rows = rows.AddRow("test", "2")
+		rows = rows.AddRow("test2", "2")
+		mock.ExpectQuery(dbSql).WillReturnRows(rows)
+		columns2 := []string{"col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8"}
+		rows2 := sqlmock.NewRows(columns2).AddRow("value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8")
+		rows2 = rows2.AddRow("value12", "value22", "value32", "value42", "value52", "value62", "value72", "value82")
+		mock.ExpectQuery(fmt.Sprintf(briefmetaSql, "'test','test2'")).WillReturnError(errors.New("failed"))
+		_, err := dbagent.GetClusterMetaData(c, true, false)
+		assert.Error(t, err)
+	})
+	t.Run("get db failed", func(t *testing.T) {
+		columns := []string{"schema_name", "t1"}
+		rows := sqlmock.NewRows(columns).AddRow("INFORMATION_SCHEMA", "1")
+		rows = rows.AddRow("test", "2")
+		rows = rows.AddRow("test2", "2")
+		mock.ExpectQuery(dbSql).WillReturnError(errors.New("failed"))
+		_, err := dbagent.GetClusterMetaData(c, true, false)
+		assert.Error(t, err)
 	})
 }
 
